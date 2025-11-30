2431c772ebafc32db04b5d2287e4d02d60b7f807
Praneeth Oruganti <sporuganti.dev@gmail.com>
2025-11-29 20:07:12 -0500

    build: migrate to Prisma v7.0.1

--- backend/prisma/migrations/20251001054311_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Definition" (
-    "id" SERIAL NOT NULL,
-    "bodyLatex" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Definition_pkey" PRIMARY KEY ("id")
-);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,15 @@
 import Fastify from 'fastify';
-import { PrismaClient } from '../generated/prisma/index.js';
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const prisma = new PrismaClient({
+  adapter,
+});
+
 const fastify = Fastify({ logger: true });
 
 // Get all definitions
